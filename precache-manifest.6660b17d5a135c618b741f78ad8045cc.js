ca39af1d3c889ab9ad86a1741a76b5abf217daba
BuildTools <unconfigured@null.spigotmc.org>
2019-04-21 18:19:30 +0500

    Updates

---
+++ precache-manifest.6660b17d5a135c618b741f78ad8045cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkapptest/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "d9fee978e18ff85fb165",
+    "url": "/vkapptest/static/js/runtime~main.d9fee978.js"
+  },
+  {
+    "revision": "9d7dfba4b24b0e979e84",
+    "url": "/vkapptest/static/js/main.9d7dfba4.chunk.js"
+  },
+  {
+    "revision": "523cd1442ee057938031",
+    "url": "/vkapptest/static/js/2.523cd144.chunk.js"
+  },
+  {
+    "revision": "9d7dfba4b24b0e979e84",
+    "url": "/vkapptest/static/css/main.62badc56.chunk.css"
+  },
+  {
+    "revision": "523cd1442ee057938031",
+    "url": "/vkapptest/static/css/2.fed72654.chunk.css"
+  },
+  {
+    "revision": "c8a8e0d59bb8b777c15ddc4137683e1e",
+    "url": "/vkapptest/index.html"
+  }
+];
